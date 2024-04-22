9b778cc57e5c8feed0ea35b48cc2f54a7c877398
TONGQIA <1227894812@qq.com>
2024-04-22 13:41:11 +0800

    调整 3

--- techie-nest-content/src/test/java/com/dango/content/service/impl/CourseBaseServiceImplTest.java
+++ techie-nest-content/src/test/java/com/dango/content/service/impl/CourseBaseServiceImplTest.java
@@ -1,13 +1,19 @@
 package com.dango.content.service.impl;
 
+import com.dango.content.model.dto.AddCourseDto;
+import com.dango.content.model.dto.CourseBaseInfoDto;
 import com.dango.content.service.CourseBaseService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author dango
@@ -31,18 +37,33 @@ class CourseBaseServiceImplTest {
         System.out.println("课程基本信息表测试结束");
     }
 
+
+
     @Test
-    void queryCoursePageList() {
-//        // 查询条件
-//        QueryCoursePageDto queryCoursePageDto = new QueryCoursePageDto();
-//        queryCoursePageDto.setCourseName("java");
-//        queryCoursePageDto.setAuditStatus("202004");
-//        queryCoursePageDto.setPublishStatus("203001");
-//        // 分页参数
-//        PageParams pageParams = new PageParams();
-//        pageParams.setPageNo(1);
-//        pageParams.setPageSize(10);
-//        PageResult<CourseBase> pageResult = courseBaseService.queryCoursePageList(pageParams, queryCoursePageDto);
-//        log.info("{}", pageResult);
+    @Transactional
+    public void testAddCourseBase() {
+        // 使用方法创建测试数据
+        AddCourseDto addCourseDto = createTestAddCourseDto();
+
+        // 调用待测试的方法
+        CourseBaseInfoDto result = courseBaseService.addCourseBase(1L, addCourseDto);
+
+        // 验证结果
+        assertNotNull(result);
     }
+
+
+    private AddCourseDto createTestAddCourseDto() {
+        AddCourseDto addCourseDto = new AddCourseDto();
+        addCourseDto.setName("测试课程");
+        addCourseDto.setMainCategory("技术");
+        addCourseDto.setSubCategory("编程");
+        addCourseDto.setIsFree(0);
+        addCourseDto.setPrice(100.0);
+        addCourseDto.setOriginalPrice(150.0);
+        addCourseDto.setPic("test.jpg");
+        // 根据需要添加其他字段
+        return addCourseDto;
+    }
+
 }
